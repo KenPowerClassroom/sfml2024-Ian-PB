16d14a8c855bf2b12ae8c144a686336601210b4b
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2024-11-13 13:54:58 +0000

    Some renaming done

--- 16_SFML_Games/tetris.cpp
+++ 16_SFML_Games/tetris.cpp
@@ -2,13 +2,13 @@
 #include <time.h>
 using namespace sf;
 
-const int M = 20;
-const int N = 10;
+const int length = 20;
+const int width = 10;
 
-int field[M][N] = {0};
+int field[length][width] = {0};
 
 struct Point
-{int x,y;} a[4], b[4];
+{ int x, y; } a[4], b[4];
 
 int figures[7][4] =
 {
@@ -18,14 +18,14 @@ int figures[7][4] =
     3, 5, 4, 7, // T
     2, 3, 5, 7, // L
     3, 5, 7, 6, // J
-    2, 3, 4, 5 // O
+    2, 3, 4, 5  // O
 };
 
 bool check()
 {
     for (int i = 0; i < 4; i++)
     {
-        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M)
+        if (a[i].x < 0 || a[i].x >= width || a[i].y >= length)
         {
             return 0;
         }
@@ -34,7 +34,6 @@ bool check()
             return 0;
         }
     }
-
 
    return 1;
 };
@@ -46,15 +45,23 @@ int tetris()
 
     RenderWindow window(VideoMode(320, 480), "The Game!");
 
-    Texture t1, t2, t3;;
-    t1.loadFromFile("images/tetris/tiles.png");
-    t2.loadFromFile("images/tetris/background.png");
-    t3.loadFromFile("images/tetris/frame.png");
+    Texture tileTexture;
+    Texture backgroundTexture;
+    Texture frameTexture;
 
-    Sprite s(t1), background(t2), frame(t3);
+    tileTexture.loadFromFile("images/tetris/tiles.png");
+    backgroundTexture.loadFromFile("images/tetris/background.png");
+    frameTexture.loadFromFile("images/tetris/frame.png");
 
-    int dx = 0; bool rotate = 0; int colorNum = 1;
-    float timer = 0, delay = 0.3;
+    Sprite tileSprite(tileTexture);
+    Sprite backgroundSprite(backgroundTexture);
+    Sprite frameSprite(frameTexture);
+
+    int speedX = 0; 
+    bool rotate = 0; 
+    int colorNum = 1;
+    float timer = 0;
+    float delay = 0.3;
 
     Clock clock;
 
@@ -62,26 +69,26 @@ int tetris()
     {
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
-        timer+=time;
+        timer += time;
 
-        Event e;
-        while (window.pollEvent(e))
+        Event event;
+        while (window.pollEvent(event))
         {
-            if (e.type == Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
 
-            if (e.type == Event::KeyPressed)
-                if (e.key.code == Keyboard::Up)
+            if (event.type == Event::KeyPressed)
+                if (event.key.code == Keyboard::Up)
                 {
                     rotate = true;
                 }
-                else if (e.key.code == Keyboard::Left)
+                else if (event.key.code == Keyboard::Left)
                 {
-                    dx = -1;
+                    speedX = -1;
                 }
-                else if (e.key.code == Keyboard::Right)
+                else if (event.key.code == Keyboard::Right)
                 {
-                    dx = 1;
+                    speedX = 1;
                 }
         }
 
@@ -90,10 +97,11 @@ int tetris()
             delay = 0.05;
         }
 
-        //// <- Move -> ///
+        //// <- Move -> ////
         for (int i = 0; i < 4; i++)
         { 
-            b[i]=a[i]; a[i].x+=dx; 
+            b[i] = a[i]; 
+            a[i].x += speedX;
         }
         if (!check())
         {
@@ -149,56 +157,58 @@ int tetris()
                 }    
             }
 
-            timer=0;
+            timer = 0;
         }
 
     ///////check lines//////////
-        int k = M - 1;
-        for (int i = M - 1; i > 0; i--)
+        int k = length - 1;
+        for (int i = length - 1; i > 0; i--)
         {
             int count=0;
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < width; j++)
             {
                 if (field[i][j]) count++;
-                field[k][j] = field[i][j];
+                {
+                    field[k][j] = field[i][j];
+                }
             }
-            if (count < N)
+            if (count < width)
             {
                 k--;
             }
         }
 
-        dx = 0;
+        speedX = 0;
         rotate = 0;
         delay = 0.3;
 
     /////////draw//////////
     window.clear(Color::White);    
-    window.draw(background);
+    window.draw(backgroundSprite);
           
-    for (int i = 0; i < M; i++)
-        for (int j = 0; j < N; j++)
+    for (int i = 0; i < length; i++)
+        for (int j = 0; j < width; j++)
         {
             if (field[i][j] == 0)
             {
                 continue;
             }
 
-            s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
-            s.setPosition(j * 18, i * 18);
-            s.move(28, 31); //offset
-            window.draw(s);
+            tileSprite.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+            tileSprite.setPosition(j * 18, i * 18);
+            tileSprite.move(28, 31); //offset
+            window.draw(tileSprite);
         }
 
         for (int i = 0; i < 4; i++)
         {
-            s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-            s.setPosition(a[i].x * 18, a[i].y * 18);
-            s.move(28, 31); //offset
-            window.draw(s);
+            tileSprite.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+            tileSprite.setPosition(a[i].x * 18, a[i].y * 18);
+            tileSprite.move(28, 31); //offset
+            window.draw(tileSprite);
         }
 
-        window.draw(frame);
+        window.draw(frameSprite);
         window.display();
     }
 
